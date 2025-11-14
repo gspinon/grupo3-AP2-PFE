346532276ea477e59dd28b708fa538cadf5d3d98
gspinon <guipinon97@gmail.com>
2025-11-14 07:31:55 -0300

    On branch main

--- src/components/CartaoProjeto.jsx
+++ src/components/CartaoProjeto.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import imagemPadrao from '../assets/profile-circle-svgrepo-com.svg';
-import '../assets'
 
 const CartaoProjeto = ({ 
   nomeProjeto, 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../estilos/Login.css';
-import '../assets'
 
 const Login = () => {
   const [cnpj, definirCnpj] = useState('');
